cc7c9b962942ee8211268e9e7544f00a9e207ece
Matheus <matheusramosbi.13@gmail.com>
2024-06-17 18:23:40 -0300

    react components

---
+++ react-components/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Form from './form/Form';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Form />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ react-components/src/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+  return <footer>Todos os direitos reservados &copy;</footer>;
+};
+
+export default Footer;
