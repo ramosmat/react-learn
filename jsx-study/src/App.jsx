54af2b684c4d6adf3675253e6af4ed57f3d7fd66
Matheus <matheusramosbi.13@gmail.com>
2024-06-17 16:09:30 -0300

    jsx study

---
+++ jsx-study/src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const App = () => {
+  const titulo = <h1>Esse é o título da página</h1>;
+  const ativo = true;
+
+  const random = Math.random();
+
+  function mostrarNome(sobrenome) {
+    return 'Matheus ' + sobrenome;
+  }
+
+  const carro = {
+    marca: 'Ford',
+    modelo: 'Mustang',
+    ano: 1964,
+  };
+
+  const estiloP = {
+    color: 'blue',
+    backgroundColor: 'yellow',
+    fontSize: '2rem',
+    padding: '10px',
+  };
+
+  return (
+    <>
+      {titulo}
+      <p>{ativo ? 'ativo' : 'inativo'}</p>
+
+      <span className={ativo ? 'ativo' : 'inativo'}>
+        {(random * 1000) / 50}
+      </span>
+
+      <p>{mostrarNome('Ramos')}</p>
+      <p style={estiloP}>
+        {carro.marca} {carro.modelo}, {carro.ano}
+      </p>
+    </>
+  );
+};
+
+export default App;
