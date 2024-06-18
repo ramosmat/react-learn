a93a29974dfb189c93a96159cb844601d886aca1
Matheus <matheusramosbi.13@gmail.com>
2024-06-18 19:15:09 -0300

    useEffect

---
+++ useEffect/src/Produto.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Produto = () => {
+  React.useEffect(() => {
+    function handleScroll(event) {
+      console.log(event);
+    }
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return <div style={{ height: '200vh' }}>Meu Produto</div>;
+};
+
+export default Produto;
