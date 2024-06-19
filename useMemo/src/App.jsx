187c160254050a346af7c93c108bb858e82c155f
Matheus <matheusramosbi.13@gmail.com>
2024-06-19 14:51:51 -0300

    useMemo & useCallback

---
+++ useMemo/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  const [contar, setContar] = React.useState(0);
+  const valor = React.useMemo(() => {
+    const localStorageItem = window.localStorage.getItem('produto');
+    // só será executado uma vez
+    console.log('teste');
+    return localStorageItem;
+  }, []);
+  console.log(valor);
+
+  return <button onClick={() => setContar(contar + 1)}>{valor}</button>;
+};
+
+export default App;
