66e4c955603440ddb964681897bb75209487a715
Matheus <matheusramosbi.13@gmail.com>
2024-06-26 20:50:56 -0300

    checkbox exercice

--- exercicios/src/App.jsx
+++ exercicios/src/App.jsx
@@ -1,77 +1,44 @@
 import React from 'react';
-import Input from './Input';
-
-// Faça um fetch (POST) para a API abaixo
-// Para a criação ser aceita é necessário enviar dodos de:
-// nome, email, senha, cep, rua, numero, bairro, cidade e estado
-
-// Essa é a função utilizado para realizar o POST
-// fetch('https://ranekapi.origamid.dev/json/api/usuario', {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   // form é o objeto com os dados do formulário
-//   body: JSON.stringify(form),
-// });
-
-// Mostre uma mensagem na tela, caso a resposta da API seja positiva
+// Otimize o código do slide anterior
+// Utilizando a array abaixo para mostrar
+// cada checkbox na tela.
 
 const App = () => {
-  const [formData, setFormData] = React.useState({});
-  const [response, setResponse] = React.useState(null);
-  const [status, setStatus] = React.useState(null);
+  const coresArray = ['azul', 'roxo', 'laranja', 'verde', 'vermelho', 'cinza'];
+  const [cores, setCores] = React.useState([]);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    setStatus('Enviando...');
-    console.log(status);
-
-    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      // form é o objeto com os dados do formulário
-      body: JSON.stringify(formData),
-    }).then((response) => {
-      setResponse(response);
-    });
+  function handleChange({ target }) {
+    if (target.checked) {
+      setCores([...cores, target.value]);
+    } else {
+      setCores(cores.filter((cor) => cor !== target.value));
+    }
   }
 
-  function onFormDataChange(id, value) {
-    setFormData({
-      ...formData,
-      [id]: value,
-    });
+  function handleChecked(cor) {
+    return cores.includes(cor);
   }
 
   return (
-    <>
-      {(response && response.ok && <p>Usuário Criado!</p>) || <p>{status}</p>}
-      <form onSubmit={handleSubmit}>
-        <Input label="Nome" type="text" onFormDataChange={onFormDataChange} />
-        <Input label="Email" type="email" onFormDataChange={onFormDataChange} />
-        <Input
-          label="Senha"
-          type="password"
-          onFormDataChange={onFormDataChange}
-        />
-        <Input label="CEP" type="text" onFormDataChange={onFormDataChange} />
-        <Input label="Rua" type="text" onFormDataChange={onFormDataChange} />
-        <Input
-          label="Numero"
-          type="number"
-          onFormDataChange={onFormDataChange}
-        />
-        <Input label="Bairro" type="text" onFormDataChange={onFormDataChange} />
-        <Input label="Cidade" type="text" onFormDataChange={onFormDataChange} />
-        <Input label="Estado" type="text" onFormDataChange={onFormDataChange} />
+    <form>
+      {coresArray.map((cores, index) => (
+        <label key={index} style={{ textTransform: 'capitalize' }}>
+          <input
+            type="checkbox"
+            value={cores}
+            checked={handleChecked(cores)}
+            onChange={handleChange}
+          />
+          {cores}
+        </label>
+      ))}
 
-        <button>Enviar</button>
-      </form>
-    </>
+      <ul>
+        {cores.map((cor) => (
+          <li key={cor}>{cor}</li>
+        ))}
+      </ul>
+    </form>
   );
 };
 
