b58c465fd6526adfb986988c29cab205b5729c78
Matheus <matheusramosbi.13@gmail.com>
2024-06-26 20:38:42 -0300

    checkbox

---
+++ forms/checkbox/src/App.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+
+const App = () => {
+  const [cores, setCores] = React.useState([]);
+
+  function handleChange({ target }) {
+    if (target.checked) {
+      setCores([...cores, target.value]);
+    } else {
+      setCores(cores.filter((cor) => cor !== target.value));
+    }
+  }
+
+  function handleChecked(cor) {
+    return cores.includes(cor);
+  }
+
+  return (
+    <form>
+      <label>
+        <input
+          type="checkbox"
+          value="azul"
+          checked={handleChecked('azul')}
+          onChange={handleChange}
+        />
+        Azul
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          value="vermelho"
+          checked={handleChecked('vermelho')}
+          onChange={handleChange}
+        />
+        Vermelho
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          value="verde"
+          checked={handleChecked('verde')}
+          onChange={handleChange}
+        />
+        Verde
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          value="amarelo"
+          checked={handleChecked('amarelo')}
+          onChange={handleChange}
+        />
+        Amarelo
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          value="roxo"
+          checked={handleChecked('roxo')}
+          onChange={handleChange}
+        />
+        Roxo
+      </label>
+      <ul>
+        {cores.map((cor) => (
+          <li key={cor}>{cor}</li>
+        ))}
+      </ul>
+    </form>
+  );
+};
+
+export default App;
