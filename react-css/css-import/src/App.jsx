aaa82bb01390dd771c5cf1ea03e501700ab8214b
Matheus <matheusramosbi.13@gmail.com>
2024-07-01 11:15:43 -0300

    css-import

---
+++ react-css/css-import/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Title from './Title';
+
+const App = () => {
+  return (
+    <>
+      <div className="AppContainer">alou</div>
+      <Title />
+    </>
+  );
+};
+
+export default App;
